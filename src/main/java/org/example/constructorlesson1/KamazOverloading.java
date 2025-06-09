bac46d507cf3d46bc7b01eaf9857e65d42015f74
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-06-09 15:18:29 +0400

    text

--- src/main/java/org/example/constructorlesson1/KamazOverloading.java
+++ src/main/java/org/example/constructorlesson1/KamazOverloading.java
@@ -1,19 +1,20 @@
 package org.example.constructorlesson1;
 
 public class KamazOverloading {
-   String transport (String qum){
-       return "Kamaz "+qum+"Getirir";
+    String transport(String qum) {
+        return "Kamaz " + qum + "Getirir";
 
-   }
-   String transport (String wuwe,String pencere){
-       return "Kamaz "+ wuwe + pencere+ " getirir ";
-   }
+    }
+
+    String transport(String wuwe, String pencere) {
+        return "Kamaz " + wuwe + pencere + " getirir ";
+    }
 
     public static void main(String[] args) {
         KamazOverloading kamaz = new KamazOverloading();
-     String ilk =kamaz.transport("qum ");
+        String ilk = kamaz.transport("qum ");
         System.out.println(ilk);
-        String ikinci=kamaz.transport(" wuwe ","pencere");
+        String ikinci = kamaz.transport(" wuwe ", "pencere");
         System.out.println(ikinci);
     }
 }
