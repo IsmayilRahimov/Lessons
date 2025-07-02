150535ec3d85cfdb77ce6249f4dc6ebf96eca9f4
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-07-03 00:59:41 +0400

    List task

--- src/main/java/org/example/listarraylist/Persons.java
+++ src/main/java/org/example/listarraylist/Persons.java
@@ -1,10 +1,16 @@
 package org.example.listarraylist;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Persons {
     public static void main(String[] args) {
         List<String> persons = List.of("Elvin", "Eli", "Musa", "Murat");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Listde bir ad secin");
+        String name = scanner.nextLine();
+        persons.add(name);
+        System.out.println(persons);
         if (persons.contains("Ismayil")) {
             System.out.println("Murat var");
         } else {
