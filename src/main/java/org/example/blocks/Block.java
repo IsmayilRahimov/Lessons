9d51f9d2eef3d107d9776869b454a743431c6abb
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-07-04 19:42:29 +0400

    Task

---
+++ src/main/java/org/example/blocks/Block.java
@@ -0,0 +1,24 @@
+package org.example.blocks;
+
+public class Block {
+    static String name = "Qasimov";
+
+
+
+    public static void main(String[] args) {
+        System.out.println(name);
+        Block block = new Block();
+
+        block.name = "Musa";
+
+
+
+        }
+        void print(){
+        System.out.println(name );
+    }
+    {
+        String name = "Musa";
+        System.out.println(name);
+    }
+}
