04b18955cdff8db402c8d0e656cdb71fae8d82ea
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-07-08 19:59:40 +0400

    edit kod // deyisiklik var 15 ci setrde :)

--- src/main/java/org/example/trycatch/TryScanCatch.java
+++ src/main/java/org/example/trycatch/TryScanCatch.java
@@ -7,19 +7,19 @@ public class TryScanCatch {
         Scanner scanner = new Scanner(System.in);
         String input = null;
 
-         try {
-             System.out.println("Yazini daxil edin: null saxlamaq ucun entere vurun : ");
-             input = scanner.nextLine();
-             if (input.equals("")) {
-                 input = null;
-                 System.out.println("Sizin metnininiz: "+ null);
-             }else {
-                 System.out.println("Sizin metnininiz: "+ input);
-             }
-         }catch (NullPointerException e){
-             System.out.println("Metnin girilmedi");
-         }finally {
-             System.out.println("Tesekkurler");
-         }
+        try {
+            System.out.println("Yazini daxil edin: null saxlamaq ucun entere vurun : ");
+            input = scanner.nextLine();
+            if (input.equals("")) {
+                input = null;
+                System.out.println("Sizin metnininiz: " + input.length());
+            } else {
+                System.out.println("Sizin metnininiz: " + input);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Hecne daxil etmediniz:");
+        } finally {
+            System.out.println("Tesekkurler");
+        }
     }
 }
