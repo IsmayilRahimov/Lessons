8c3c8cbccdf831b0e818ae25b15ad0c371fec289
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-07-08 18:13:36 +0400

    Task

---
+++ src/main/java/org/example/pytn/Nurlana.java
@@ -0,0 +1,19 @@
+package org.example.pytn;
+
+import java.util.Scanner;
+
+public class Nurlana {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Eded daxil et:");
+        int number = scanner.nextInt();
+        int sum = 0;
+        System.out.println("Diger ededi daxil et:");
+        int number2 = scanner.nextInt();
+        sum = number + number2;
+        System.out.println("Toplam: "+sum);
+
+
+
+    }
+}
