d051d453769739bf1ec342371fdf6377c2c4c2cf
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-07-13 17:57:03 +0400

    Algoritmler // oz uzerimde praktika

---
+++ src/main/java/org/example/algorithm/BubleSortExample.java
@@ -0,0 +1,21 @@
+package org.example.algorithm;
+
+import java.util.Arrays;
+
+public class BubleSortExample {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 7, 9, 6, 5};
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    System.out.println(Arrays.toString(arr));
+                }
+            }
+        }
+    }
+}
