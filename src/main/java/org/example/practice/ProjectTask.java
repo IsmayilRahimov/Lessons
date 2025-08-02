c8be93c3ddf1d7b2937e6ecc32e8a373fe319da4
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-03 02:17:31 +0400

    Practice // task

---
+++ src/main/java/org/example/practice/ProjectTask.java
@@ -0,0 +1,29 @@
+package org.example.practice;
+
+public class ProjectTask {
+    public static void main(String[] args) {
+
+        int number = 10;
+        int age;
+        age = number;
+        System.out.println(age);
+
+        int age1;
+        age1 = 10;
+        System.out.println(age1);
+        byte length = 10;
+        System.out.println(length);
+        short length1 = 10;
+        System.out.println(length1);
+        int result = length + length1;
+        System.out.println(result);
+
+        int million_number = 1_000_000_000;
+        int result1 = million_number * 1000000;
+        System.out.println(result1);
+
+        double price;
+        price = 17.2;
+        System.out.println(price);
+    }
+}
