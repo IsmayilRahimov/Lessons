f0fa6982228612c6397e558fd2bb9cceb65d1a7c
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-03 03:56:42 +0400

    Operators task and experience myself practice//

---
+++ src/main/java/org/example/practice/OperatorTask.java
@@ -0,0 +1,21 @@
+package org.example.practice;
+/*
+
+ */
+
+/**
+ *
+ */
+public class OperatorTask {
+    public static void main(String[] args) {
+
+
+        int a = 23;
+        int b = 5;
+        int result = a % b;  // 23 % 5 = qaliq 3
+        System.out.println(result);
+        double result1 =(double) a / b;
+        System.out.println(result1);
+
+    }
+}
