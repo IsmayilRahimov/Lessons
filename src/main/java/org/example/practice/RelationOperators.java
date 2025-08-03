5381fb2c6676accfe1488b96205994e63124ad3c
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-03 23:44:40 +0400

    relation operators ///*

---
+++ src/main/java/org/example/practice/RelationOperators.java
@@ -0,0 +1,11 @@
+package org.example.practice;
+
+public class RelationOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        boolean result = (a < b) && (b < c);
+        System.out.println(result);
+    }
+}
