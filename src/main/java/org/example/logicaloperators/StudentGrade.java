d44a0ecd39b7aa8bc8f938cb567a2a75cc63d1b2
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-04 01:44:19 +0400

    logical operators//

--- src/main/java/org/example/logicaloperators/StudentGrade.java
+++ src/main/java/org/example/logicaloperators/StudentGrade.java
@@ -2,9 +2,16 @@ package org.example.logicaloperators;
 
 public class StudentGrade {
     public static void main(String[] args) {
-        int grade = 51;
-        boolean isNotPass = grade < 50 && grade > 40;
-
-        System.out.println(isNotPass);
+//        int grade = 51;
+//        boolean isNotPass = grade < 50 && grade > 40;
+//
+//        System.out.println(isNotPass);
+//
+//        int num = 80;
+//        boolean isOdd = (num % 2 == 0) ? false : true;
+//        System.out.println(isOdd);
+        int gradewith = 85;
+        char grade = gradewith < 81  ? 'A' : 'B';
+        System.out.println(grade);
     }
 }
