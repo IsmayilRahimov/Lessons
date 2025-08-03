5d5f170b68f61950bcdafd51daf825d03e7b9382
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-04 00:33:46 +0400

    operators /*

--- src/main/java/org/example/practice/RelationOperators.java
+++ src/main/java/org/example/practice/RelationOperators.java
@@ -2,10 +2,24 @@ package org.example.practice;
 
 public class RelationOperators {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        boolean result = (a < b) && (b < c);
-        System.out.println(result);
+//        int a = 10;
+//        int b = 20;
+//        int c = 30;
+//        boolean result = (a < b) && (b < c);
+//        System.out.println(result);
+
+        short grade1 = 10;
+        short grade2 = 20;
+
+        boolean result1 = grade1 == grade2;
+        System.out.println(result1);
+        boolean result2 = grade1 != grade2;
+        System.out.println(result2);
+        boolean reult = grade1 != grade2;
+        System.out.println(reult);
+        boolean result3 = grade1 > grade2;
+        System.out.println(result3);
+        System.out.println(grade1 < grade2);
+        System.out.println(grade1 <= grade2);
     }
 }
