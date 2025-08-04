69854fa115f168f210ae2ea9407308f5ad140e3f
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-04 22:33:53 +0400

    scanner, switch case ,calculator

---
+++ src/main/java/org/example/practice/ScannerProject.java
@@ -0,0 +1,33 @@
+package org.example.practice;
+
+import java.util.Scanner;
+
+public class ScannerProject {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter first numbers:");
+        int num1 = scanner.nextInt();
+        System.out.print("Please enter second numbers:");
+        int num2 = scanner.nextInt();
+        System.out.println("Please enter the operator: [ + , - , /, *, % ]");
+        char operator = scanner.next().charAt(0);
+        switch (operator) {
+            case '+':
+                System.out.println("Sum is " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Subtraction is " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Multiplication is " + (num1 * num2));
+                break;
+            case '/':
+                System.out.println("Division is " + (num1 / num2));
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+
+
+    }
+}
