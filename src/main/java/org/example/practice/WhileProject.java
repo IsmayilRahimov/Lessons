1afe5dfe8ce5a367153d288fa3095776940d8c79
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-04 23:28:14 +0400

    While loop my practice//

---
+++ src/main/java/org/example/practice/WhileProject.java
@@ -0,0 +1,19 @@
+package org.example.practice;
+/*
+
+ */
+
+/**
+ * salam Java
+ */
+public class WhileProject {
+    public static void main(String[] args) {
+        int i = 0;
+        //        true   false = qaliqsiz bolunende 1de dayanir
+        while (i < 5 && i % 2 == 0) {
+
+            i++;
+        }
+        System.out.println(i);
+    }
+}
