e7e55433a75ff1be22a9a75d71634aaff9df3be6
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-04 17:28:55 +0400

    scanner if //

--- src/main/java/org/example/practice/ScanProject.java
+++ src/main/java/org/example/practice/ScanProject.java
@@ -8,12 +8,18 @@ public class ScanProject {
         System.out.println("Ededi daxil et:");
 
         int num = sc.nextInt();
-
+//          eger qaliqsiz bolunurse
         if (num % 2 == 0) {
             System.out.println("true");
-
+//         eger qaliqsiz bolunmurse
         } else if (num % 2 != 0) {
-            System.out.println("false");
+            System.out.println("false " + num + " Edediniz tekdir:");
+        }
+
+        if (num % 2 == 0) {
+            System.out.println(num + " Edediniz cutdur");
+        } else {
+            System.out.println(num + " Edediniz tekdir");
         }
     }
 }
