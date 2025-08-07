7baff0699dee2fb0f36fbe76694b61f0f8e702de
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-07 18:08:13 +0400

    for loop do while//array

---
+++ src/main/java/org/example/Foorloop/LoopFor.java
@@ -0,0 +1,13 @@
+package org.example.Foorloop;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int a = 0; a < 10; a++) {
+                System.out.println(a);
+            }
+            System.out.println();
+            System.out.print("Salam " + i );
+        }
+    }
+}
