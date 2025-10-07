04ed067d9b7408f8aa84d700b1f1da69e2799d12
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-07 19:22:02 +0400

    oop objects

---
+++ src/main/java/org/example/op/Ab4.java
@@ -0,0 +1,8 @@
+package org.example.op;
+
+public class Ab4 extends AbstractClassPr {
+    @Override
+    void eat() {
+        System.out.println("Ab4 eat ");
+    }
+}
---
+++ src/main/java/org/example/op/AbstractClassPr.java
@@ -0,0 +1,8 @@
+package org.example.op;
+
+public abstract class AbstractClassPr {
+
+    abstract void eat();
+
+
+}
