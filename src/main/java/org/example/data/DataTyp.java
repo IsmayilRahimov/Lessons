0ab02a52d7a1dc279367e8af50b546665df13ac9
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-07 19:46:26 +0400

    data types

---
+++ src/main/java/org/example/data/DataTyp.java
@@ -0,0 +1,16 @@
+package org.example.data;
+
+public class DataTyp {
+
+    public static void main(String[] args) {
+        int age = 17;
+        boolean isStudent = true;
+        double salary = 1700.5;
+        char grade = 'A';
+
+        System.out.println(age + " Age");
+        System.out.println(isStudent + " Students?");
+        System.out.println(salary + " Salary");
+        System.out.println(grade + " Uni check grade :");
+    }
+}
