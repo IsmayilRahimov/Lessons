b5e0dfae95e118f2ba066b9e27b7324fe335062a
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-12 18:28:57 +0400

    array min max practice .forme

---
+++ src/main/java/org/example/Foorloop/ArrForRay.java
@@ -0,0 +1,10 @@
+package org.example.Foorloop;
+
+public class ArrForRay {
+    public static void main(String[] args) {
+        int[] arr = {1, 9, 6, 3, 5, 4, 7, 8};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("indeks :" + i + "= element : " + arr[i]);
+        }
+    }
+}
---
+++ src/main/java/org/example/Foorloop/LoopK.java
@@ -0,0 +1,15 @@
+package org.example.Foorloop;
+
+public class LoopK {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " :bu eded cutdur");
+            } else {
+                System.out.println(i + " :bu eded tekdir");
+            }
+
+        }
+    }
+}
