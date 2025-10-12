f1deca70ac30e8df514258cfbecabf9ecf59b0b4
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-12 19:00:05 +0400

    String

---
+++ src/main/java/org/example/constructoranot/StringHr.java
@@ -0,0 +1,18 @@
+package org.example.constructoranot;
+
+public class StringHr {
+
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+
+        String a1 = new String("Pop");
+        String a2 = new String("Pop");
+
+        System.out.println(s1 == s2); // burada String pool baxir gorur eyni "Java" oldugu ucun artiq yeni yaratmir eyni obyekte isare edir:
+        System.out.println(s1.equals(s2));
+
+        System.out.println(a1 == a2); // burada ise yeni obyektler yarandigi ucun
+    }
+}
