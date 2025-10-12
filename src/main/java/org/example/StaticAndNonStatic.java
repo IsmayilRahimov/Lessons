60ffd563d6cb211f1b6376c0c209022069205bb1
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-13 00:57:23 +0400

    Static and non static practice for me

---
+++ src/main/java/org/example/StaticAndNonStatic.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class StaticAndNonStatic {
+    static String name = "Anar";
+    String surname = "Ragimov";
+
+    static void show() {
+        System.out.println("Ad " + name + " Static class seviyesinde:");//static
+    }
+
+    void sleep() {
+        System.out.println("Soyad " + surname + " -Static name elave oluna biler:" + name); // non static method cagirilanda static filds cagirmaq mumkundur:
+    }
+
+
+}
+
+class MainA {
+    public static void main(String[] args) {
+        StaticAndNonStatic.show();
+        StaticAndNonStatic s1 = new StaticAndNonStatic();
+        s1.sleep();
+        s1.surname.length();
+    }
+
+}
